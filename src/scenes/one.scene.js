8c73cf2be1cf30fee463e58e9fe00166d1a58eca
Noman5237 <anonyman637@gmail.com>
2022-04-19 19:24:30 +0600

    one scene

--- src/scenes/one.scene.js
+++ src/scenes/one.scene.js
@@ -1,4 +1,4 @@
-import { ECS, GameManager, Entities, Components } from 'miaam';
+import { ECS, GameManager, Entities, Components, Systems } from 'miaam';
 import Player from '../entities/player.js';
 
 class One extends ECS.Scene {
@@ -7,8 +7,11 @@ class One extends ECS.Scene {
 		const map = new Entities.Map('/assets/tilemaps/scene1.tilemap.json');
 		this.addEntity(map);
 		const player = new Player({ x: 50, y: 50 });
-		const camera = new Components.Camera({ x: 100, y: 100 });
+		const camera = new Components.Camera({ width: 100, height: 100 });
+		this.addEntity(player);
 		player.addComponent(camera);
+		this.addSystem({ id: 'renderer', system: new Systems.Renderer() });
+
 		// GameManager.instance.app.stage.addChild(view.props.map);
 	}
 
