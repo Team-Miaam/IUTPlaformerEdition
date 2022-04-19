b9ec536b2337b501fa1e632c643e07c4799f2c9a
Noman5237 <anonyman637@gmail.com>
2022-04-20 01:15:26 +0600

    player animation done

--- src/scenes/one.scene.js
+++ src/scenes/one.scene.js
@@ -2,18 +2,16 @@ import { ECS, GameManager, Entities, Components, Systems } from 'miaam';
 import Player from '../entities/player.js';
 
 class One extends ECS.Scene {
+	player;
+
 	constructor() {
 		super();
 		const map = new Entities.Map('/assets/tilemaps/scene1.tilemap.json');
 		this.addEntity(map);
-		const player = new Player({ x: 500, y: 500 });
-		const gameScreen = GameManager.instance.app.screen;
-		const camera = new Components.Camera({ width: gameScreen.width, height: gameScreen.height });
-		this.addEntity(player);
-		player.addComponent(camera);
-		this.addSystem({ id: 'renderer', system: new Systems.Renderer() });
 
-		// GameManager.instance.app.stage.addChild(view.props.map);
+		this.player = new Player({ x: 100, y: 100 });
+		this.addEntity(this.player);
+		this.addSystem({ id: 'renderer', system: new Systems.Renderer() });
 	}
 
 	update() {
