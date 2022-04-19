51531d6de020399c96e5d3122cd2d5db15480bc3
Noman5237 <anonyman637@gmail.com>
2022-04-19 17:42:03 +0600

    camera systems

--- src/index.mjs
+++ src/index.mjs
@@ -11,5 +11,6 @@ GameManager.instance.createApplication({
 
 const { default: SceneOne } = await AssetsManager.instance.importChunk({ chunk: SceneOneChunk });
 SceneManager.instance.scene = new SceneOne();
+SceneManager.instance.start();
 
-SceneManager.instance.scene = document.getElementById('root').appendChild(GameManager.instance.window);
+document.getElementById('root').appendChild(GameManager.instance.window);
--- src/scenes/one.scene.js
+++ src/scenes/one.scene.js
@@ -1,17 +1,19 @@
-import { ECS, AssetsManager } from 'miaam';
-
-const { getResource } = AssetsManager.instance;
+import { ECS, GameManager, Entities, Components } from 'miaam';
+import Player from '../entities/player.js';
 
 class One extends ECS.Scene {
-	#map = getResource('/assets/tilemaps/scene1.tilemap.json');
-
 	constructor() {
 		super();
-		console.log(this.#map);
+		const map = new Entities.Map('/assets/tilemaps/scene1.tilemap.json');
+		this.addEntity(map);
+		const player = new Player({ x: 50, y: 50 });
+		const camera = new Components.Camera({ x: 100, y: 100 });
+		player.addComponent(camera);
+		// GameManager.instance.app.stage.addChild(view.props.map);
 	}
 
 	update() {
-		console.log('hello');
+		super.update();
 	}
 }
 
